5c3ec860d896c8bcade69be5ca5990dc612b4885
Darya Yurenka <dareybeen@gmail.com>
2025-10-30 21:46:50 +0500

    t3

--- lesson3/3.py
+++ lesson3/3.py
@@ -5,3 +5,9 @@
 	- цена продажи со скидкой 10%
 	- цена продажи со скидкой 15%
 '''
+smartphone_price = int(input('smartphone price = '))
+
+print(smartphone_price + smartphone_price * 0.3)
+print(smartphone_price - smartphone_price * 0.05)
+print(smartphone_price - smartphone_price * 0.1)
+print(smartphone_price - smartphone_price * 0.15)

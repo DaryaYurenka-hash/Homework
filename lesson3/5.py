3178f412694b17b2e77a8a01775cbf4720d8fbc4
Darya Yurenka <dareybeen@gmail.com>
2025-10-30 22:35:52 +0500

    t5

--- lesson3/5.py
+++ lesson3/5.py
@@ -4,3 +4,9 @@
 Пример: 35457 -> 09:50:57
 Сделать 2 варианта с форматной строкой и без.
 '''
+
+seconds = int(input('Seconds = '))
+hours = seconds // 3600
+minutes = (seconds % 3600) // 60
+seconds = seconds % 60
+print(f'{hours}:{minutes}:{seconds}')

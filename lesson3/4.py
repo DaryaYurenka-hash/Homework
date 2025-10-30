c28c4985f517e73aab147b93b404fbf7b2fd6031
Darya Yurenka <dareybeen@gmail.com>
2025-10-30 22:10:44 +0500

    t4

--- lesson3/4.py
+++ lesson3/4.py
@@ -14,4 +14,9 @@
 '''
 
 
-
+number = int(input('Enter a number = '))
+thousands = number // 1000
+hundreds = (number // 100) % 10
+tens = (number // 10) % 10
+ones = number % 10
+print(f'Thousands - {thousands}, Hundreds - {hundreds}, Tens - {tens}, Ones - {ones}', sep='\n')

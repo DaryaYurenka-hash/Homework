a4bd1431cc2522381424335e5924225a9bbb3124
Darya Yurenka <dareybeen@gmail.com>
2025-11-05 00:20:22 +0500

    t2 lesson4

--- lesson4/homework/str_2.py
+++ lesson4/homework/str_2.py
@@ -5,3 +5,9 @@
     - выдать среднее арифметическое
 
 '''
+count_of_numbers = input("Введите числа через пробел ")
+numbers_list = list(map(int, count_of_numbers.split()))
+sum_of_numbers = sum(numbers_list)
+max_number = max(numbers_list)
+arr_numbers = sum(numbers_list) / len(numbers_list)
+print(f'Сумма чисел -  {sum_of_numbers}, максимальное число из списка - {max_number}, среднее арифметическое - {arr_numbers}')

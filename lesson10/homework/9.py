dc2b15a82d67f7001596bf81669ab0661a29a988
Darya Yurenka <dareybeen@gmail.com>
2025-12-07 00:01:29 +0500

    lesson 10 t9

--- lesson10/homework/9.py
+++ lesson10/homework/9.py
@@ -8,3 +8,23 @@
 После выполнения функции напечатать строку "<имя функции> - завершена"
 """
 
+def log_decorator(func): #
+    def wrapper(*args): #предположим, что мы не знаем, какие аргументы будут представлены в func, тогда *args, **kwargs
+        # Указываем строки принта в будущем порядке вывода
+        print(f'Выполняеся функция {func.__name__} с аргументами {args}') #
+        result = func(*args) 
+        print(f"{func.__name__} - завершена")
+        return result # wrapper возвращает result
+    return wrapper # а декоратор возвращает wrapper
+
+# Оборачиваем в декоратор рандомную функцию
+@log_decorator
+def invitation(name, surname):
+    print(f'{name} {surname} you are a magician.')
+
+# Вызываем аргументы через строку ввода
+name = input('Name: ')
+surname = input('Surname: ')
+
+# Применяем функцию приветствия
+invitation(name, surname)

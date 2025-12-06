e2ec397c389b32c7f94fabfb4ad1fbe6d10131f0
Darya Yurenka <dareybeen@gmail.com>
2025-12-06 22:51:50 +0500

    lesson10 t6

--- lesson10/homework/6.py
+++ lesson10/homework/6.py
@@ -16,4 +16,24 @@ next(tn_gen) -> 15
 next(tn_gen) -> 21
 """
 
+def triangular_numbers(n: int):
+    if not isinstance(n, int):
+        raise TypeError("Введите целое число.")
+    if n < 1:
+        raise ValueError("n должно быть больше или равно 1.")
 
+    for k in range(1, n + 1):
+        tr = k * (k + 1) // 2
+        yield tr
+# Генератор считает треугольное число для каждого n от 1 до заданного значения. 
+# При применении next мы получаем каждое значение до указанного нами n
+
+
+gen = triangular_numbers(7)
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))

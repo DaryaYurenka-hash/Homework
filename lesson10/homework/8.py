b8c521eb21dab37092cebf58256351ccb0816304
Darya Yurenka <dareybeen@gmail.com>
2025-12-09 14:08:47 +0500

    lesson 10 t8

--- lesson10/homework/8.py
+++ lesson10/homework/8.py
@@ -7,3 +7,27 @@
 * сделать настраиваемы параметр который определяет печать в консоль или в файл
 и если в файл передать название фала
 """
+
+def decorate_err(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as err:
+            return print(f'Функция {func.__name__} не выполнена по причине: {err}.')
+    return wrapper
+
+@decorate_err
+def check(a: int):
+    if not isinstance(a, int):
+        raise TypeError ('Ошибка ввода данных, введите данные типа int')
+    return print(a)
+
+a = 12.5
+
+check(a)
+
+
+
+
+
+
